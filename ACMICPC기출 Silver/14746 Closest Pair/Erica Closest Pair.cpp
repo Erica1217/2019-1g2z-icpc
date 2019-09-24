b7aed940666dd0c7ba8998b8237b73092c04983a
김유진 <yyjin1217@naver.com>
2019-09-24 14:40:52 +0900

    .

---
+++ ACMICPC기출 Silver/14746 Closest Pair/Erica Closest Pair.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+int main()
+{
+    int pn,qn;
+    int c1,c2;
+    int P[510000]={0,};
+    int Q[510000]={0,};
+    cin>>pn>>qn;
+    cin>>c1>>c2;
+    for(int i=0; i<pn ; i++)
+    {
+        cin>>P[i];
+    }
+    for(int i=0; i<qn ; i++)
+    {
+        cin>>Q[i];
+    }
+    
+    sort(P,P+pn);
+    sort(Q,Q+qn);
+    
+    if(P[pn-1] < Q[0])
+    {
+        int distance = abs(c1-c2)+abs(Q[0]-P[pn-1]);
+        cout<<distance<<" 1";
+        return 0;
+    }
+    
+    if(Q[qn-1]<P[0])
+    {
+        int distance = abs(c1-c2)+abs(Q[qn-1]-P[0]);
+        cout<<distance<<" 1";
+        return 0;
+    }
+    
+
+    int i=0,j=1;
+    long long min=9999876543210;
+    int result=0;
+    while(i!=pn && j!=qn)
+    {
+        if(Q[j-1]<=P[i] && Q[j] >= P[i] )
+        {
+            if(min>abs(P[i]-Q[j]))
+            {
+                min=abs(P[i]-Q[j]);
+                result=1;
+            }
+            else if(min==abs(P[i]-Q[j]))
+            {
+                result++;
+            }
+            
+            if(min>abs(Q[j-1]-P[i]))
+            {
+                min=abs(Q[j-1]-P[i]);
+                result=1;
+            }
+            else if(min==abs(Q[j-1]-P[i]))
+            {
+                result++;
+            }
+            i++;
+        }
+        else if(Q[j-1]>P[i] && Q[j] >= P[i])
+        {
+            i++;
+        }else
+        {
+            j++;
+        }
+    }
+    
+    if(i==j && min>abs(P[pn-1]-Q[qn-1]))
+    {
+        min=abs(P[pn-1]-Q[qn-1]);
+        result=1;
+    }
+    else if(min==abs(P[pn-1]-Q[qn-1]))
+    {
+        result++;
+    }
+    
+    long long distance = abs(c1-c2)+abs(min);
+    cout<<distance<<" "<<result;
+}
